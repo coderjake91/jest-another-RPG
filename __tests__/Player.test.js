676307d65bfd0ce2b65ec6da964b1f7450def56a
coderjake91 <jacob.b.frazer@gmail.com>
2021-09-05 15:08:10 -0500

    created additional properties and methods for the Player constructor, such as inventory, getStats(), and getInventory, tested the correct instantiation of the Player constructor using Player test jest file

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,3 @@
-const { expect } = require('@jest/globals');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
@@ -6,6 +5,7 @@ jest.mock('../lib/Potion.js');
 
 console.log(new Potion());
 
+//check for the correct creation of the Player object
 test('create a player object', () => {
     const player = new Player('Dave');
 
@@ -19,4 +19,27 @@ test('create a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+//check for the correct retrieval of player stats data using getStats() player method
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    //check that player.getStats() method returns an object with four properties
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+//checks for the correct retrieval of player inventory using getInventory() method
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    //check to see if getInventory returns false if player.inventory is an empty []
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
