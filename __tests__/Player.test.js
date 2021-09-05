05ba21470d9ac585ced329d6f6ded463544e29e7
coderjake91 <jacob.b.frazer@gmail.com>
2021-09-05 16:05:59 -0500

    updated Player constructor to have additioal methods of getHealth, isAlive, and reduceHealth- created tests to confirm that these methods function as expected

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -43,3 +43,35 @@ test('gets inventory from player or returns false', () => {
 
     expect(player.getInventory()).toEqual(false);
 });
+
+//test getHealth() method of Player
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+//tests isAlive() method of Player
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+//tests to confirm reduceHealth() Player method is correctly deducting health points from the player properly
+test("substracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
